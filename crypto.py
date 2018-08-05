fa512f496d2b633ca401c7ee8b2ddb172c8f2f09
CYB3RMX <42123683+CYB3RMX@users.noreply.github.com>
2018-08-06 01:21:04 +0300

    Update crypto.py

--- crypto.py
+++ crypto.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
 import hashlib
-print("CRYPTO PROGRAM by CYB3RMX_")
-print("==========================")
+print("\u001b[36mCRYPTO PROGRAM by CYB3RMX_")
+print("\u001b[37m==========================")
 print("CURRENT CRYPTO ALGORITHMS")
 print("[1] MD5")
 print("[2] SHA256")
@@ -10,16 +10,17 @@ print("[3] SHA512")
 select = int(input("SELECT ALGORITHM: "))
 message = str(input("INPUT YOUR STRING MESSAGE: "))
 if select == 1:
-  print("==========================")
+  print("\033[32m==========================")
   print(hashlib.md5(b"message").hexdigest())
   print("==========================")
 elif select == 2:
-  print("==========================")
+  print("\033[32m==========================")
   print(hashlib.sha256(b"message").hexdigest())
   print("==========================")
 elif select == 3:
-  print("==========================")
+  print("\033[32m==========================")
   print(hashlib.sha512(b"message").hexdigest())
   print("==========================")
 else:
+  print("\u001b[31m=========================")
   print("NEW ALGORITHMS COMING SOON...")
